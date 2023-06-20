09b11b4faa3e9f87c7a419fec0d2632c01b90d05
posin <dlawogud517@naver.com>
2023-06-21 04:10:10 +0900

    백업용

---
+++ app/src/main/java/com/example/travelday_2/Country.kt
@@ -0,0 +1,7 @@
+package com.example.travelday_2
+
+data class Country(
+    val name: String = "",
+    val startDate: String = "",
+    val endDate: String = ""
+)
--- app/src/main/java/com/example/travelday_2/TravelListAdapter.kt
+++ app/src/main/java/com/example/travelday_2/TravelListAdapter.kt
@@ -1,16 +1,16 @@
-package com.example.travelday
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.travelday_2.databinding.TravelListRowBinding
 
-class TravelListAdapter(val items: ArrayList<String>) :RecyclerView.Adapter<TravelListAdapter.ViewHolder>() {
+
+class TravelListAdapter(private val items: ArrayList<String>) : RecyclerView.Adapter<TravelListAdapter.ViewHolder>() {
 
     interface OnItemClickListener{
         fun OnItemClick(data: String)
     }
     var itemClickListener:OnItemClickListener?=null
+
     inner class ViewHolder(val binding: TravelListRowBinding) : RecyclerView.ViewHolder(binding.root){
         init{
             binding.textView.setOnClickListener{
@@ -41,7 +41,8 @@ class TravelListAdapter(val items: ArrayList<String>) :RecyclerView.Adapter<Trav
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.binding.textView.text = item
-        holder.binding.dDay.text=""  // 나중에 구현
+        holder.binding.dDay.text = ""
+        // 나중에 구현
     }
 
     fun setData(newData: ArrayList<String>) {
